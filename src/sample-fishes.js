1ff7e824c14f0bf202d1a60807262edcb3430a48
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-09 12:47:17 -0500

    This version works deploying to firebase

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -2,7 +2,7 @@
 const fishes = {
   fish1: {
     name: 'Pacific Halibut',
-    image: '/static/media/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
+    image: '/images/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
     desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
     price: 1724,
     status: 'available'
@@ -10,7 +10,7 @@ const fishes = {
 
   fish2: {
     name: 'Lobster',
-    image: '/static/media/stock-photo-32135274-cooked-lobster.jpg',
+    image: '/images/stock-photo-32135274-cooked-lobster.jpg',
     desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
     price: 3200,
     status: 'available'
@@ -18,7 +18,7 @@ const fishes = {
 
   fish3: {
     name: 'Sea Scallops',
-    image: '/static/media/stock-photo-58624176-scallops-on-black-stone-plate.jpg',
+    image: '/images/stock-photo-58624176-scallops-on-black-stone-plate.jpg',
     desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
     price: 1684,
     status: 'unavailable'
@@ -26,7 +26,7 @@ const fishes = {
 
   fish4: {
     name: 'Mahi Mahi',
-    image: '/static/media/stock-photo-12556651-mahimahi.jpg',
+    image: '/images/stock-photo-12556651-mahimahi.jpg',
     desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
     price: 1129,
     status: 'available'
@@ -34,7 +34,7 @@ const fishes = {
 
   fish5: {
     name: 'King Crab',
-    image: '/static/media/stock-photo-18294110-king-crab-legs.jpg',
+    image: '/images/stock-photo-18294110-king-crab-legs.jpg',
     desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
     price: 4234,
     status: 'available'
@@ -42,7 +42,7 @@ const fishes = {
 
   fish6: {
     name: 'Atlantic Salmon',
-    image: '/static/media/stock-photo-56241842-salmon-fish.jpg',
+    image: '/images/stock-photo-56241842-salmon-fish.jpg',
     desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
     price: 1453,
     status: 'available'
@@ -50,7 +50,7 @@ const fishes = {
 
   fish7: {
     name: 'Oysters',
-    image: '/static/media/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
+    image: '/images/stock-photo-58626682-fresh-oysters-on-a-black-stone-plate-top-view.jpg',
     desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
     price: 2543,
     status: 'available'
@@ -58,7 +58,7 @@ const fishes = {
 
   fish8: {
     name: 'Mussels',
-    image: '/static/media/stock-photo-40450406-steamed-mussels.jpg',
+    image: '/images/stock-photo-40450406-steamed-mussels.jpg',
     desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
     price: 425,
     status: 'available'
@@ -66,7 +66,7 @@ const fishes = {
 
   fish9: {
     name: 'Jumbo Prawns',
-    image: '/static/media/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
+    image: '/images/stock-photo-67121439-fresh-tiger-shrimp-on-ice-on-a-black-stone-table.jpg',
     desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
     price: 2250,
     status: 'available'
